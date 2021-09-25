cdc276d273b29afdc5c38a6c08e751bbdaa054c4
PurviMec <princy.mecwan94@gmail.com>
2021-09-24 21:16:38 -0400

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,32 @@
-
+const inputEl = document.getElementById('input-field');
+const timeEl = document.getElementById('time-block');
+const presentDate = document.getElementById('currentDay');
 
 // show current date in header
-
 let m = moment();
 document.getElementById('currentDay').innerHTML = moment().format('dddd, MMMM Do');
 
 
 //color coded past future and present time
+var currentHour = moment().format('H A');
+console.log(currentHour);
+
+
+    
 
 //add list in event when typed
-var tasks = function(){
-    
-}
+
+
 
 //save list item in local storage and stays there even refreshed page
-let input = document.getElementById('input-field');
-let time = document.getElementById('time-block');
-var saveTasks = function(){
-    localStorage.setItem('input', 'time', JSON.stringify(input, time));
-}
 $("#save-btn").click(function(){
+   
     saveTasks();
 });
+
+var saveTasks = function(){
+    localStorage.setItem('To Do', JSON.stringify($('input-field')));
+}
+
+
+localStorage.getItem($("input-field"));
